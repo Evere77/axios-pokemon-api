5517f382f366daa604067632c3e3463a8a7e7a26
Evere77 <evere777@yahoo.com>
2023-05-09 11:38:08 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// worked with Kevin
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  // state var
+  const [pokemons, setPokemons] = useState([])
+
+  // GET AXIOS
+  const axiosPokemons = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+        .then( (serverRes) => {
+          console.log(serverRes)
+          // AXIOS WRAPS its response inside a 'data' key
+          setPokemons(serverRes.data.results)
+        })
+        .catch(err => console.log(err))
+  }
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon API</h1>
+      <button onClick={axiosPokemons}>AXIOS Pokemon</button>
+      {/* <p>{JSON.stringify(pokemons)}</p> */}
+      {pokemons.map((poke, idx) => {
+        return (
+        <p key={idx}><li>{poke.name}</li></p>
+        )
+      })
+      }
     </div>
+    </>
   );
 }
 
